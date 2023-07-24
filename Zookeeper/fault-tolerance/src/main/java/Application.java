351660aad84d145cc1bd4bf27b023c215d707c4e
Faizan Patel <faizanpatel@Faizans-MacBook-Pro-14.local>
2023-07-24 22:32:13 +0100

    Fault Tolerant cluster class added and used in Application main class

---
+++ Zookeeper/fault-tolerance/src/main/java/Application.java
@@ -0,0 +1,32 @@
+import org.apache.zookeeper.ZooKeeper;
+import zk.classes.FaultTolerantCluster;
+
+import javax.naming.InsufficientResourcesException;
+import java.io.IOException;
+import org.apache.zookeeper.KeeperException;
+
+public class Application {
+
+    private static final String ZNODE_ROOT = "/election";
+    private ZooKeeper zkConnection;
+
+    public static void main(String[] args) throws IOException, InterruptedException, KeeperException, InsufficientResourcesException {
+
+        Application app = new Application();
+        app.initCluster();
+        app.waitForEventThread();
+    }
+
+    // Initialise a zookeeper cluster
+    private void initCluster() throws IOException, InterruptedException, KeeperException, InsufficientResourcesException {
+        FaultTolerantCluster cluster = new FaultTolerantCluster();
+        cluster.initialiseCluster(ZNODE_ROOT);
+        this.zkConnection = cluster.getConnection();
+        cluster.addNewZNode();
+    }
+    private void waitForEventThread() throws InterruptedException {
+        synchronized (zkConnection) {
+            zkConnection.wait();
+        }
+    }
+}
--- Zookeeper/fault-tolerance/src/main/java/zk/classes/FaultTolerantCluster.java
+++ Zookeeper/fault-tolerance/src/main/java/zk/classes/FaultTolerantCluster.java
@@ -1,48 +1,78 @@
+package zk.classes;
+
 import org.apache.zookeeper.*;
 import org.apache.zookeeper.data.Stat;
-import zk.classes.ZKConnection;
 
+import javax.naming.InsufficientResourcesException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 
-public class FaultTolerance implements Watcher { // define a Zookeeper Watcher(event-handler) class
-    private static final String ZNODE_ROOT = "/election";
+public class FaultTolerantCluster implements Watcher { // define a Zookeeper Watcher(event-handler) class
+    private String rootZNodePath;
     private ZooKeeper zookeeper; // zookeeper client object
     private String currentZnodeName;  // current znode's name
 
-    // NOTE - Don't forget to create the /election ZNode using zkCli.sh before executing this method
-    public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
-        FaultTolerance election = new FaultTolerance();
-        election.connectToZookeeper();
-        election.createZNode();
-        election.electLeaderWithFailOver();
-        election.waitForEventThread();
-        System.out.println();
-        System.out.println("Disconnected from zookeeper, exiting application!");
+    // Initialise a zookeeper cluster with given Znode root
+    public void initialiseCluster(String znodeRoot) throws InterruptedException, KeeperException {
+        connectToZookeeper();
+        setRootZNode(znodeRoot);
     }
 
-    private void connectToZookeeper() throws IOException {
-        this.zookeeper = new ZKConnection().getConnection();
-    }
-
-    // wait for zookeeper event threads to finish
-    private void waitForEventThread() throws InterruptedException
-    {
-        synchronized (zookeeper) {
-            this.zookeeper.wait(); // main threads waits for the event thread to finish
+    // establish connection to zookeeper and return the connection object
+    public void connectToZookeeper() {
+        if (zookeeper == null) {
+            try {
+                this.zookeeper = new ZKConnection().getConnection();
+            } catch (IOException ex) {
+                System.out.println("Can not connect to ZooKeeper server!");
+            }
         }
     }
 
-    // This method creates a new znode (i.e. a ZK client) with a new sequence id assigned by the ZK server.
-    private void createZNode() throws InterruptedException, KeeperException {
-        String znodePrefix = ZNODE_ROOT + "/c_"; // full path prefix for creating new modes ("c_" = child node)
+    public ZooKeeper getConnection() {
+        return zookeeper;
+    }
+
+    // set root-level znode for the cluster
+    public void setRootZNode(String rootName) throws InterruptedException, KeeperException {
+
+        // connect to zookeeper if not connected already
+        connectToZookeeper();
+
+        final String rootPath = rootName.startsWith("/") ? rootName : "/" + rootName; // set the root path
+
+        // create a parchment root node if it doesn't exist already
+        Stat stat = zookeeper.exists(rootPath, this);
+        if (stat == null) {
+            zookeeper.create(rootPath, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+        }
+        rootZNodePath = rootPath;
+    }
+
+    public String getRootZNodePath() {
+        return rootZNodePath;
+    }
+
+    // This method creates a new znode under the root with a new sequence id assigned by the ZK server.
+    // It also queries for the leader or a watcher node in the existing cluster after creating one
+    // returns - newly created znode's name
+    public String addNewZNode() throws InterruptedException, KeeperException, InsufficientResourcesException {
+
+        if (rootZNodePath.isEmpty()) {
+            throw new InsufficientResourcesException("Can not create a new znode! Please set the Root ZNode first!");
+        }
+
+        String znodePrefix = rootZNodePath + "/c_"; // full path prefix for creating new modes ("c_" = child node)
         String znodeFullPath = zookeeper.create(znodePrefix, new byte[]{}, ZooDefs.Ids.OPEN_ACL_UNSAFE,
                                             CreateMode.EPHEMERAL_SEQUENTIAL);   // creates a new temporary (ephemeral) znode
                                                                                 // using given prefix and appending it with a new sequence id
 
-        System.out.println("znode full name: " + znodeFullPath);
-        this.currentZnodeName = znodeFullPath.replace(ZNODE_ROOT + "/", ""); // extract only the name (without its path)
+        System.out.println("znode's full path: " + znodeFullPath);
+        this.currentZnodeName = znodeFullPath.replace(rootZNodePath + "/", ""); // extract only the name (without its path)
+
+        electLeaderWithFailOver();
+        return currentZnodeName;
     }
 
     /**
@@ -60,9 +90,9 @@ public class FaultTolerance implements Watcher { // define a Zookeeper Watcher(e
         Stat stats = null;
         String predecessorName = "";
 
-        // keep repeating the loop until we are the leader or we find a valid znode to watch
+        // keep repeating the loop until we are the leader, or we find a valid znode to watch
         while (stats == null) {
-            List<String> children = zookeeper.getChildren(ZNODE_ROOT, false);
+            List<String> children = zookeeper.getChildren(rootZNodePath, false);
             Collections.sort(children); // sort the nodes by their sequence (names)
             // now if the smallest child is same as the current znode then it is the leader
             String smallestNode = children.get(0);
@@ -75,11 +105,12 @@ public class FaultTolerance implements Watcher { // define a Zookeeper Watcher(e
                 // find the previous (predecessor) znode
                 int predecessorIndex = Collections.binarySearch(children, currentZnodeName) - 1;
                 predecessorName = children.get(predecessorIndex);
-                stats = zookeeper.exists(ZNODE_ROOT + "/" + predecessorName, this); // watch the predecessor
+                stats = zookeeper.exists(rootZNodePath + "/" + predecessorName, this); // watch the predecessor
             }
         }
 
         System.out.println("Watching node: " + predecessorName);
+        System.out.println();
     }
 
     // implement event handler method 'process' of Watcher Interface (to handle zookeeper events)
